5cba3491f9e6df748d11089c759b233c4860376a
Shristy1503 <shriimishra15@gmail.com>
2025-03-06 23:31:11 +0530

    [Shristy] Add.UC3 Functionality

--- HelloWorldApp/src/app/app.component.ts
+++ HelloWorldApp/src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  message: string = "Hello from BridgeLabz"; // ✅ Declare message property
+  message: string = "Hello from BridgeLabz"; 
+  logoPath: string  = "assets/BL_logo_square_jpg.jpg";  
 }
