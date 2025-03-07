761eb2126b22096ba3e2bdd7ab3f761fca37015c
Shristy1503 <shriimishra15@gmail.com>
2025-03-07 10:46:26 +0530

    [Shristy] Add.UC5 Functionality

--- HelloWorldApp/src/app/app.component.ts
+++ HelloWorldApp/src/app/app.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, FormsModule],  
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  userName: string = 'Shristy Mishra';  
+
   openBridgeLabz() {
     window.open('https://www.bridgelabz.com', '_blank');
-  } 
+  }
 }
